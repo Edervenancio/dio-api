6f70ef48b76ae24fc33039a54c7c026c47c301ed
Eder venancio <ederpvenancio@gmail.com>
2023-10-09 15:26:55 -0300

    add model

--- src/main/java/dio/web/api/Model/Usuario.java
+++ src/main/java/dio/web/api/Model/Usuario.java
@@ -1,2 +1,27 @@
-package dio.web.api.Model;public class Usuario {
+package dio.web.api.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+    private Integer id;
+    private String login;
+    private String password;
+
+    public Usuario(){}
+
+    public Usuario(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
